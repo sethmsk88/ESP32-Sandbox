5c7bbe7bf2ae61da1c19adc1a3687542018c1369
sethmsk88 <sethmsk88@yahoo.com>
2024-01-08 07:44:49 -0500

    Toggle LED strip with button

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
+#include <FastLED.h>
 #include "OTA.h"
 
-#define LED_PIN 9
+#define DATA_PIN 9
 #define BTN_PIN 10
+#define NUM_LEDS 47
 
 int ledState = LOW; // tracks the current state of LED
 int lastButtonState; // the previous state of button
 int currentButtonState; // the current state of button
+
+CRGB leds[NUM_LEDS];
+CRGB color;
 
 void setup() {
   Serial.begin(115200);
@@ -14,22 +19,21 @@ void setup() {
   setupOTA();
 
   pinMode(BTN_PIN, INPUT);  // Set button pin to input mode
-  pinMode(LED_PIN, OUTPUT); // Set LED pin to output mode
+
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 800);
+  FastLED.setBrightness(20);
+
+  color = CRGB::DarkRed;
+
+  fill_solid(leds, NUM_LEDS, color);
+  FastLED.show();
 
   currentButtonState = digitalRead(BTN_PIN);
 }
 
-void blinkLED() {
-  delay(1000);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-}
-
 void loop() {
   ElegantOTA.loop();
-
-  //blinkLED();
 
   lastButtonState    = currentButtonState;                // save the last state
   currentButtonState = digitalRead(BTN_PIN); // read new state
@@ -39,19 +43,17 @@ void loop() {
   
     // toggle state of LED
     if(ledState == LOW) {
-       ledState = HIGH;  
-       Serial.println("Turning LED on");
+      ledState = HIGH;
+      fill_solid(leds, NUM_LEDS, color);
+      Serial.println("Turning LED on");
     }
     else {
       ledState = LOW;
+      fill_solid(leds, NUM_LEDS, CRGB::Black);
       Serial.println("Turning LED off");
     }
- 
-    // control LED arccoding to the toggled state
-    digitalWrite(LED_PIN, ledState);  //turns the LED on or off based on the variable
 
-    
+    FastLED.show();
   }
-  
 }
 
